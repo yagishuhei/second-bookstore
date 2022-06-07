68d33cfa5e4fb9da55c5b285a254c313b1e955ab
S.yag <shuhei.y3166@gmail.com>
2022-06-07 03:02:45 +0000

    [Add]end_usersの新規登録画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+
+#会員側
+# URL /end_users/sign_in...
+#会員側のコントローラーがどこに存在するかを指定
+  devise_for :end_users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+
+#管理者側
+# URL /admin/sign_in...
+#管理者側のコントローラーがどこに存在するかを指定
+#新規登録、パスワード変更はしないためskip オプションで削除
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
