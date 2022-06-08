d356317343e3a94872088b3e6cc3f97ab6ebb879
S.yag <shuhei.y3166@gmail.com>
2022-06-08 08:04:47 +0000

    [Update]book_column(楽天ブックスの本詳細を保存する用)

--- db/migrate/20220607042714_create_reviews.rb
+++ db/migrate/20220607042714_create_reviews.rb
@@ -2,6 +2,13 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
 
+      t.integer :end_user_id
+      t.integer :book_id
+      t.integer :category_id
+      t.string :heading
+      t.text :blog
+      t.float :rate
+
       t.timestamps
     end
   end
--- db/migrate/20220607042749_create_categories.rb
+++ db/migrate/20220607042749_create_categories.rb
@@ -2,6 +2,8 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
 
+      t.string :name
+
       t.timestamps
     end
   end
