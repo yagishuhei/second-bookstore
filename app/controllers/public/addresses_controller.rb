d64998291146833407bf6dbe2a0377c9fa92d334
S.yag <shuhei.y3166@gmail.com>
2022-06-13 06:10:27 +0000

    [Add]addresses_controller

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,10 +1,24 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = current_end_user.addresses
   end
 
-  def show
+  def create
+    @address = current_end_user.addresses.new(address_params)
+    @address.save
+    redirect_to addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
-end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def address_params
+    params.require(:address).permit(:name)
+  end
