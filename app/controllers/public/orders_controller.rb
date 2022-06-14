82abd215b826150d6f72e5b248d38de8270045a0
S.yag <shuhei.y3166@gmail.com>
2022-06-14 05:45:16 +0000

    [Finish]cart_item/index, カート商品合計、パス指定

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,23 @@
 class Public::OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+  
+  def create
+    @order = current_end_user.orders.new(order_params)
+  end
   def index
+  
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
   end
 end
