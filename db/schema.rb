23367989c460debbc66141199d58c4213e5d3ae2
yagishuhei <shuhei.y3166@gmail.com>
2022-07-04 05:58:21 +0000

    [Add]Google Natural Language API

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_03_102025) do
+ActiveRecord::Schema.define(version: 2022_07_04_053512) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -162,6 +162,7 @@ ActiveRecord::Schema.define(version: 2022_07_03_102025) do
     t.integer "review_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "rate", precision: 5, scale: 3
   end
 
   create_table "sales", force: :cascade do |t|
